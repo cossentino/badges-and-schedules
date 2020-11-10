eeb83a51f85227d566009f16fdca54d2760ad5b7
Ian Cossentino <icossentino@gmail.com>
2020-11-10 09:24:55 -0500

    commit tree 3859f8924320e03d978c64f6f3edb0502b3328ca

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(n_array)
+    badge_messages = []
+    n_array.each do |name|
+        badge_messages.push(badge_maker(name))
+    end
+    badge_messages
+end
+
+def assign_rooms(n_array)
+    rooms_list = []
+    n_array.each do |speaker|
+        rooms_list.push("Hello, #{speaker}! You'll be assigned to room #{n_array.index(speaker) + 1}!")
+    end
+    rooms_list
+end
+
+def printer(n_array)
+    badge_messages = batch_badge_creator(n_array)
+    rooms_list = assign_rooms(n_array)
+    badge_messages.each do |badge|
+        puts badge
+    end
+    rooms_list.each do |speaker|
+        puts speaker
+    end
+end
+    
+
